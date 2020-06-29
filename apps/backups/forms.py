1a2111bf195ac68829a0ae8849837682b8d927f3
Shekhar Chatterjee <shekhar.c@fyle.in>
2020-06-29 07:43:42 -0400

    merged fyled and complete states

--- apps/backups/forms.py
+++ apps/backups/forms.py
@@ -11,12 +11,11 @@ class ExpenseForm(forms.Form):
     ]
 
     expense_state_choices = [
-        ('FYLED', 'FYLED'),
+        ('COMPLETE', 'FYLED'),
         ('PAID', 'PAID'),
         ('APPROVED', 'APPROVED'),
         ('DRAFT', 'DRAFT'),
         ('APPROVER_PENDING', 'APPROVER PENDING'),
-        ('COMPLETE', 'COMPLETE'),
         ('PAYMENT_PROCESSING', 'PAYMENT PROCESSING'),
         ('PAYMENT_PENDING', 'PAYMENT PENDING')]
 
